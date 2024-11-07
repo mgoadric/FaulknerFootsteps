dcc91d523725cc872480b091c6f8871f0e367536
Sadie Lewis <LewisSN@hendrix.edu>
2024-11-07 01:13:00 -0600

    Listed sites now have cards

--- faulkner_footsteps/lib/list_item.dart
+++ faulkner_footsteps/lib/list_item.dart
@@ -4,27 +4,33 @@ import 'package:faulkner_footsteps/info_text.dart';
 import 'package:flutter/material.dart';
 import 'widgets.dart';
 
-class ListItem extends StatelessWidget{
+class ListItem extends StatelessWidget {
   ListItem({required this.siteInfo});
   HistSite siteInfo;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      child: Column(
-        children: [
-          Text(siteInfo.name),
-          const SizedBox(width: 4, height: 10),
-          Text(siteInfo.description),
-          IconButton(
-            onPressed: () {
-              AppRouter.navigateTo(context, "/hist", arguments: {"info":siteInfo});
-            },
-            icon: const Icon(Icons.arrow_circle_right_outlined)
-          )
-        ],)
-      ,
+    return Card(
+      color: const Color.fromARGB(99, 193, 117, 46),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 10.0),
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Column(
+            children: [
+              Text(siteInfo.name),
+              const SizedBox(width: 4, height: 10),
+              Text(siteInfo.description),
+              IconButton(
+                  onPressed: () {
+                    AppRouter.navigateTo(context, "/hist",
+                        arguments: {"info": siteInfo});
+                  },
+                  icon: const Icon(Icons.arrow_circle_right_outlined))
+            ],
+          )),
     );
   }
 }
