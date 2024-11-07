d579410424f9a0ae3c4df79d1745cb1c7eaa5a61
Sadie Lewis <LewisSN@hendrix.edu>
2024-11-07 01:45:08 -0600

    New fonts and colors for list page

--- faulkner_footsteps/lib/list_item.dart
+++ faulkner_footsteps/lib/list_item.dart
@@ -2,6 +2,7 @@ import 'package:faulkner_footsteps/app_router.dart';
 import 'package:faulkner_footsteps/hist_site.dart';
 import 'package:faulkner_footsteps/info_text.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'widgets.dart';
 
 class ListItem extends StatelessWidget {
@@ -11,7 +12,7 @@ class ListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: const Color.fromARGB(99, 193, 117, 46),
+      color: const Color.fromARGB(97, 216, 150, 89),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
@@ -20,9 +21,19 @@ class ListItem extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
           child: Column(
             children: [
-              Text(siteInfo.name),
+              Text(
+                siteInfo.name,
+                style: GoogleFonts.ultra(
+                    textStyle: const TextStyle(
+                        color: Color.fromARGB(255, 124, 54, 16))),
+              ),
               const SizedBox(width: 4, height: 10),
-              Text(siteInfo.description),
+              Text(
+                siteInfo.description,
+                style: GoogleFonts.rakkas(
+                    textStyle: const TextStyle(
+                        color: Color.fromARGB(255, 175, 92, 48))),
+              ),
               IconButton(
                   onPressed: () {
                     AppRouter.navigateTo(context, "/hist",
--- faulkner_footsteps/lib/pages/list_page.dart
+++ faulkner_footsteps/lib/pages/list_page.dart
@@ -4,19 +4,19 @@ import 'package:faulkner_footsteps/app_state.dart';
 import 'package:faulkner_footsteps/hist_site.dart';
 import 'package:faulkner_footsteps/info_text.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:faulkner_footsteps/list_item.dart';
 
-class ListPage extends StatefulWidget{
+class ListPage extends StatefulWidget {
   ListPage({super.key});
 
   ApplicationState app_state = ApplicationState();
-  
+
   @override
   State<ListPage> createState() => _ListPageState();
 }
 
 class _ListPageState extends State<ListPage> {
-
   void _incrementCounter() {
     setState(() {
       //HistSite newSite = HistSite(name: "Example piece 3", description: "This is an example piece for this presentation we are making. This is a description of the universe within our feeble mortal minds.",blurbs: [InfoText(title: "This is a tittle for this section of the thing", value: "This is a short description blurb.", date: "The dates can be any string for the sake of flexibility."), InfoText(title: "This is a very long section to show possibilities", value: "GibbirishGibbirishGibbirishGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbiririshGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirishGibbirish")], images: []);
@@ -25,18 +25,15 @@ class _ListPageState extends State<ListPage> {
   }
 
   void _update(Timer timer) {
-    setState(() {
-      
-    });
+    setState(() {});
   }
+
   late Timer updateTimer;
   @override
   void initState() {
     super.initState();
     updateTimer = Timer.periodic(Duration(milliseconds: 500), _update);
-    setState(() {
-      
-    });
+    setState(() {});
   }
 
   @override
@@ -49,21 +46,25 @@ class _ListPageState extends State<ListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("This is a display page for historical sites in faulkner county"),
+        title: Text(
+          "Display page for hist sites",
+          style: GoogleFonts.ultra(
+              textStyle:
+                  const TextStyle(color: Color.fromARGB(255, 124, 54, 16))),
+        ),
       ),
       body: Column(
         children: [
           SizedBox(
-            height: 500,
-            child: ListView.builder(
-                itemCount: widget.app_state.historicalSites.length,
-                itemBuilder: (BuildContext context, int index) {
-                  print("${widget.app_state.historicalSites.length} eldritch");
-                  HistSite site = widget.app_state.historicalSites[index];
-                  return ListItem(siteInfo: site);
-                }
-              )
-          )
+              height: 500,
+              child: ListView.builder(
+                  itemCount: widget.app_state.historicalSites.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    print(
+                        "${widget.app_state.historicalSites.length} eldritch");
+                    HistSite site = widget.app_state.historicalSites[index];
+                    return ListItem(siteInfo: site);
+                  })),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -73,5 +74,4 @@ class _ListPageState extends State<ListPage> {
       ),
     );
   }
-
 }
