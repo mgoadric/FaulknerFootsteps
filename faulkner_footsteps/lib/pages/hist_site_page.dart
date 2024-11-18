166b7bdd066983c06399bfd70a78751d43462670
Sadie Lewis <LewisSN@hendrix.edu>
2024-11-18 01:19:48 -0600

    Added ratings to hist site

--- faulkner_footsteps/lib/pages/hist_site_page.dart
+++ faulkner_footsteps/lib/pages/hist_site_page.dart
@@ -14,23 +14,28 @@ class HistSitePage extends StatefulWidget {
 
 class _HistSitePage extends State<HistSitePage> {
   Future<void> showRatingDialog() async {
-    await showDialog<double>(
+    final double? userRating = await showDialog<double>(
       context: context,
       builder: (BuildContext context) => const RatingDialog(),
     );
+    if (userRating != null) {
+      setState(() {
+        widget.histSite.updateRating(userRating);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 219, 196, 166),
+      backgroundColor: const Color.fromARGB(255, 219, 192, 158),
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 218, 180, 130),
+        backgroundColor: const Color.fromARGB(255, 184, 162, 135),
         title: Text(
           widget.histSite.name,
           style: GoogleFonts.ultra(
               textStyle:
-                  const TextStyle(color: Color.fromARGB(255, 124, 54, 16))),
+                  const TextStyle(color: Color.fromARGB(255, 49, 29, 21))),
         ),
       ),
       body: SingleChildScrollView(
@@ -45,9 +50,11 @@ class _HistSitePage extends State<HistSitePage> {
                 itemCount: 3,
                 itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image(image: AssetImage('assets/images/placeholder.png')),  //PLACEHOLDER IMAGE
-                  );
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                          image: AssetImage(
+                              'assets/images/placeholder.png')) //PLACEHOLDER IMAGE
+                      );
                 },
               ),
               // child: ListView.builder(
@@ -61,6 +68,22 @@ class _HistSitePage extends State<HistSitePage> {
               //   },
               // ),
             ),
+
+            const SizedBox(height: 8.0),
+            // Display average rating
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Center(
+                child: Text(
+                  "Average Rating: ${widget.histSite.avgRating.toStringAsFixed(1)} (${widget.histSite.ratingAmount} ratings)",
+                  style: GoogleFonts.ultra(
+                      textStyle: const TextStyle(
+                          color: Color.fromARGB(255, 49, 29, 21),
+                          fontSize: 12.0)),
+                ),
+              ),
+            ),
+
             const SizedBox(height: 16.0),
             // Display blurbs (title, value, and date)
             Padding(
@@ -76,17 +99,17 @@ class _HistSitePage extends State<HistSitePage> {
                         Text(infoText.title,
                             style: GoogleFonts.ultra(
                                 textStyle: const TextStyle(
-                                    color: Color.fromARGB(255, 124, 54, 16)))),
+                                    color: Color.fromARGB(255, 49, 29, 21)))),
                         Text(infoText.value,
                             style: GoogleFonts.rakkas(
                                 textStyle: const TextStyle(
-                                    color: Color.fromARGB(255, 175, 92, 48)))),
+                                    color: Color.fromARGB(255, 92, 54, 40)))),
                         if (infoText.date != "")
                           Text(
                             "Date: ${infoText.date}",
                             style: GoogleFonts.acme(
                                 textStyle: const TextStyle(
-                                    color: Color.fromARGB(255, 124, 54, 16))),
+                                    color: Color.fromARGB(255, 49, 29, 21))),
                           ),
                         const Padding(padding: EdgeInsets.only(bottom: 25.0)),
                       ],
@@ -102,19 +125,19 @@ class _HistSitePage extends State<HistSitePage> {
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.3), 
-              blurRadius: 10.0,                     
-              offset: const Offset(3, 3),                  
+              color: Colors.black.withOpacity(0.3),
+              blurRadius: 10.0,
+              offset: const Offset(3, 3),
             ),
           ],
-          borderRadius: BorderRadius.circular(30),   
+          borderRadius: BorderRadius.circular(30),
         ),
         child: FloatingActionButton(
-          backgroundColor: const Color.fromARGB(255, 218, 180, 130),
+          backgroundColor: const Color.fromARGB(255, 49, 29, 21),
           onPressed: showRatingDialog,
           child: const Icon(
             Icons.star,
-            color: Color.fromARGB(255, 124, 54, 16),
+            color: Color.fromARGB(255, 218, 180, 130),
             size: 30,
           ),
         ),
