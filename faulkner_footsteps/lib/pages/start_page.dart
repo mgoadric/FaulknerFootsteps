04dc57027103e67ba4d56dc0b4e13c58fc20753d
Mason Murphy <murphyma@hendrix.edu>
2024-12-15 19:41:01 -0600

    Made some style changes, still working on buttons

--- faulkner_footsteps/lib/pages/start_page.dart
+++ faulkner_footsteps/lib/pages/start_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:faulkner_footsteps/app_router.dart';
@@ -29,8 +31,31 @@ class _StartPageState extends State<StartPage> with SingleTickerProviderStateMix
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await player.setSourceAsset('audio/GuitarStrumFF.mp3');
       await player.resume();
+
+      Future.delayed(Duration(seconds: 3), () {
+        fadeOutAudio();
+      });
     });
   }
+
+  // Handles the fade out of the Guitar strum
+  void fadeOutAudio() {
+  const steps = 20;
+  const stepDuration = Duration(milliseconds: 100);
+  double volume = 1.0;
+
+  Timer.periodic(stepDuration, (timer) {
+    volume -= 1 / steps;
+    if (volume <= 0) {
+      player.stop();
+      timer.cancel();
+    } else {
+      player.setVolume(volume);
+    }
+  });
+}
+
+
 
   void _handleContinue() async {
   player.stop(); // Stop the audio
@@ -45,44 +70,60 @@ class _StartPageState extends State<StartPage> with SingleTickerProviderStateMix
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 72, 52, 52),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          
-            FadeTransition(
-              opacity: _animation,
-              child: Image.asset(
-                'assets/images/FFSplash.png',
-                width: 350, // Adjust the size as needed
-                height: 350,
-              ),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: _handleContinue,
-              child: const Text('Continue'),
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(200, 50),
-                 // Adjust button size as needed
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _handleContinue,
-              child: const Text('Login'),
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(200, 50),
-              ),
-            ),
-          ],
-        ),
+    return FadeTransition(
+    opacity: _animation,
+    child: AnimatedContainer(
+      duration: Duration(seconds: 5),
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          center: Alignment.center,
+          radius: 12,
+          colors: [
+          Color.fromARGB(255, 72, 52, 52),
+          Color.fromARGB(255, 184, 141, 106),
+          ]
+        )
       ),
-    );
-  }
-
+    
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(175.0),
+                  child: Image.asset(
+                    'assets/images/FFSplash.png',
+                    width: 350, // Adjust the size as needed
+                    height: 350,
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: _handleContinue,
+                child: const Text('Continue'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 50),
+                  // Adjust button size as needed
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _handleContinue,
+                child: const Text('Login'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 50),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    )
+  );
+}
+  
   @override
   void dispose() {
     _controller.dispose();
