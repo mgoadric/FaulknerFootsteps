61969dd1a46e2899dc52ff4b89203fde7191a2f4
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2025-02-04 12:16:31 -0600

    Added location handling

--- faulkner_footsteps/lib/pages/map_display.dart
+++ faulkner_footsteps/lib/pages/map_display.dart
@@ -13,6 +13,27 @@ class MapDisplay extends StatefulWidget {
   @override
   _MapDisplayState createState() => _MapDisplayState();
 }
+Future<Position> _getlocation() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+       return Future.error('Location permissions are denied');
+    }
+  }
+   if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  }
+  return await Geolocator.getCurrentPosition();
+
+}
 
 class _MapDisplayState extends State<MapDisplay> {
   final Map<String, LatLng> siteLocations = {
